f8c224fea3d5a964e72323c236b60a550d01fab7
joselevelsup <joselevelsup@gmail.com>
2024-10-22 17:42:40 -0400

    adding comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider  } from "@tanstack/react-query";
 import Index from "./pages/index";
 import Todo from "./pages/todo";
 
+//The query client is an instance of a client that will contain any cache and configuration that we use through out the app.
+const client = new QueryClient();
+
+//This function is going to set up our routes for the Single Page Application
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/todo/:id" element={<Todo />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={client}>
+      {/*The query client provider will make the client available to us whenever we use useQuery in any of our routes*/}
+      <BrowserRouter>
+        {/* BrowserRouter stores the current location in the browser's address bar using clean URLs and navigates using the browser's built-in history stack.*/}
+        <Routes>
+          {/* Routes will match a set of child routes from the current location.*/}
+          {/* Route will allow us to couple the path to a component we want*/}
+          <Route path="/" element={<Index />} />
+          <Route path="/todo/:id" element={<Todo />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,6 @@
+//Always remember that all hooks in react start with "use"
+import { useQuery, useMutation } from "@tanstack/react-query";
+
 export default function Index() {
   return (
     <div className="text-center">This is where all the todos will live</div>
